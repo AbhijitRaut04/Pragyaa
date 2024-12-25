558493d5c7b2e524c34391185421f8a3d683bd58
Nandkishor Jadhav <nandkishorjadhav9580@gmail.com>
2024-12-25 15:41:45 +0530

    faq page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './pages/Home/Home';
+import FAQPage from './Faq/Faq';
 
 const App = () => {
   return (
     <div>
       <Home />
+      <FAQPage />
     </div>
   )
 }
